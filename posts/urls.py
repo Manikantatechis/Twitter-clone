c2ec5c7300c66dc50f1e90b95f271c581a356310
benbobyabraham <benbobyabraham@gmail.com>
2022-02-04 14:15:01 +0530

    updating files

---
+++ posts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path("edit/<int:post_id>/", views.edit, name="edit"),
+    path('delete/<int:post_id>/', views.delete, name='delete'),
+    path('like/<int:post_id>/', views.like, name='like'),
+    
+
+]
